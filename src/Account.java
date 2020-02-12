43403a72eef8a602ce6221d754494b16a19de568
kangbo@ssfs.org <Elly122913!>
2020-02-12 09:48:56 -0500

    Completed

--- src/Account.java
+++ src/Account.java
@@ -4,15 +4,57 @@ public class Account {
     private int acctNum;
     private double balance;
 
-    public Account (String f, String l, double b){
+    public Account (String first, String last, double balance){
         acctNum = (int)(Math.random()*999)+1;
         this.first=first;
         this.last=last;
         this.balance=balance;
     }
-    public Account (Account dupacc) {
-        this(dupacc.first, dupacc.last, dupacc.balance);
+    public Account (Account acc) {
+        this(acc.first, acc.last, acc.balance);
     }
+    public Account (){
+        this.first=" ";
+        this.last=" ";
+        this.balance=0;
+    }
+    public void setFirst(String first){
+        this.first=first;
+    }
+    public void setLast(String last){
+        this.last=last;
+    }
+    public void setBalance(double balance){
+        this.balance=balance;
+    }
+    public int getAcctNum(){
+        return acctNum;
+    }
+    public String getFirst(){
+        return first;
+    }
+    public String getLast(){
+        return last;
+    }
+    public double getBalance(){
+        return balance;
+    }
+    public void deposit(double a){
+        balance+=a;
+    }
+    public boolean withdrawal(double a){
+        if(balance-a>=0){
+            balance-=a;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public String toString(){
+        return "Consumer Name: "+first+" "+last+"\nAccount #:"+acctNum+"\nCurrent Balance: $"+balance;
+    }
+
 
 
 
--- src/Truck.java
+++ src/Truck.java
@@ -7,16 +7,64 @@ public class Truck {
     private static double totalFuel;
 
     public Truck (String t){
-        truckID = t;
+        truckID=t;
     }
-    public Truck (double o, double m, double f){
-        odometer = o;
-        mpg = m;
-        fuel = f;
+    public Truck (String t, double o, double m, double f){
+        truckID=t;
+        odometer=o;
+        mpg=m;
+        fuel=f;
     }
-    public Truck () {
-        odometer = 0.0;
-        mpg =0.0;
-        fuel = 0.0;
+    public String getTruckID(){
+        return truckID;
     }
+    public double getOdometer(){
+        return odometer;
+    }
+    public double getMpg(){
+        return mpg;
+    }
+    public double getFuel(){
+        return fuel;
+    }
+    public void setMpg(double mpg){
+        this.mpg=mpg;
+    }
+    public boolean enoughFuel(double m){
+        if(m<(m/mpg)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public String drive(double m){
+        if(enoughFuel(m)){
+            odometer+=m;
+            fuel-=(m/mpg);
+            return "Success";
+        }
+        else{
+            return "Truck "+truckID+" does not have enough fuel to drive "+m+" miles";
+        }
+    }
+    public void fill(){
+        fuel+=(double)CAPACITY-fuel;
+        totalFuel+=fuel;
+    }
+    public String fill(double g){
+        if(CAPACITY-fuel<g){
+            return "Truck "+truckID+": Gallons exceeds tank capacity";
+        }
+        else{
+            return "Success";
+        }
+    }
+    public static double getTotalFuel(){
+        return (int)totalFuel;
+    }
+    public String toString(){
+        return "Truck: "+truckID+"\nOdometer: "+odometer+"\nMiles Per Gallon: "+mpg+"\nFuel: "+fuel;
+    }
+
 }
